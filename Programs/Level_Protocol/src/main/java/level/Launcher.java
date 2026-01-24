a45cb994b967fdf42926e879def44f579c798557
Repeating1212 <khoo99.bi@gmail.com>
2026-01-24 18:24:03 +0800

    Feat: Add runnable file for Level

---
+++ Programs/Level_Protocol/src/main/java/level/Launcher.java
@@ -0,0 +1,10 @@
+package level;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Suppress warnings
+        System.setProperty("javafx.verbose", "false");
+        System.setProperty("prism.verbose", "false");
+        MainApp.main(args);
+    }
+}
